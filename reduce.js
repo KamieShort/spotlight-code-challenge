1f7c7d782b43c4b7f8333992d23f62f4c7dc42de
Kamie Short <kamie.short@gmail.com>
2022-03-31 11:53:56 -0700

    spotlight pair code challenge

--- reduce.js
+++ reduce.js
@@ -2,9 +2,15 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  return brands.reduce((acc, brand) => [...acc, ...brand.shoes], []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+
+export const getAverageAge = (dogs) => {
+  const age = [6, 3, 4, 2, 1];
+  return age.reduce((acc, age) => (acc += age / 5), 0);
+};
